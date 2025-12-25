df796924f54b7d3dbaeeb0927368adbba34f4f0c
razaross444 <razaross@comcast.net>
2025-12-25 00:33:15 -0500

    updated emotion_detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,10 +2,11 @@ import requests, json
 
 def emotion_detector(text_to_analyze):
 
+    # No validation or error handling yet
+    
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = {"raw_document": { "text": text_to_analyze }}
 
     response = requests.post(url, json = input_json, headers=header)
-    
     return response.text
