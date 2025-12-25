2363b7aa444a5c096f5cb7610578f302c5be7111
razaross444 <razaross@comcast.net>
2025-12-25 04:11:36 -0500

    added api and error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,9 +1,7 @@
 import requests, json
 
 def emotion_detector(text_to_analyze):
-
-    # No validation or error handling yet
-
+    
     # request config
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -11,6 +9,15 @@ def emotion_detector(text_to_analyze):
 
     # request to watson api
     response = requests.post(url, json = input_json, headers=header)
+
+    # test status code
+    if response.status_code == 400:
+        return  {"anger" : None, 
+                 "disgust" : None,
+                 "fear" : None,
+                 "joy" : None,
+                 "sadness" : None,
+                 "dominant_emotion" : None}
 
     # convert response to dictionary
     dict_response = json.loads(response.text)
